e45406f8a4475944a34e997ea0b4f318db31c989
Benjaminoyarzun <104220399+Benjaminoyarzun@users.noreply.github.com>
2025-02-27 00:40:28 -0300

    27/02/25 0:37 wordsearch function lista
    busqueda de palabras configurada para buscar todas las instancias de una palabra ignorando mayúsculas y minúsculas en una url deseada. Añadido también análisis de clases, tipos de etiqueta padres y hermanos.

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
+
+# - - - - - - Scrapers de precios - - - - - -
 def MercadoLibrepriceScrap():
     url = "https://www.mercadolibre.com.ar/ofertas#nav-header"
 
@@ -50,5 +52,42 @@ def TiendaMiapriceScrap():
         else: 
             print('error') 
             print(response.status_code)
-TiendaMiapriceScrap()
-#polla
+
+#- - - - - - Busqueda por palabras - - - - - - 
+def wordSearch():
+    url = "https://tiendamia.com/ar/tiendamia-en-vivo"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+    }
+    # set headers
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            palabra= input("Ingrese la palabra o frase a encontrar en la page: ").lower()
+            numero_apariciones=0
+            instancias = soup.find_all(text=lambda t: t and palabra in t.lower())
+            for instancia in instancias:
+                numero_apariciones=numero_apariciones+1
+                tag = instancia.parent
+    
+                # Imprimir información sobre el elemento
+                
+                print("-Etiqueta:", tag.name)
+                print("-Clases:", tag.get('class'))
+                print("-Padre:", tag.parent.name if tag.parent else "Raíz")
+                hermanos = [hermano.name for hermano in tag.parent.contents if hermano != tag and hermano.name]
+                if hermanos:
+                    print("-Hermanos:", hermanos)
+                else:
+                    print("-Hermanos: Sin hermanos")
+                print("-Texto:", tag.get_text(strip=True))
+                print("-" * 50)
+
+            print(f"Esta palabra aparece un total de {numero_apariciones} veces a lo largo de la pagina especificada")
+    
+    else: 
+            print('error') 
+            print(response.status_code)    
+wordSearch()
